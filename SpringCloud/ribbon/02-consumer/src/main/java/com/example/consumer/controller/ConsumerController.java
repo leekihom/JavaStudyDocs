00338c1661f5856e7e81b247f6af556f0407323b
leezihong <2578527296@qq.com>
2022-12-11 21:14:50 +0800

    ribbon

---
+++ SpringCloud/ribbon/02-consumer/src/main/java/com/example/consumer/controller/ConsumerController.java
@@ -0,0 +1,39 @@
+package com.example.consumer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author leezihong
+ * @date 2022/12/11 17:11
+ * @description
+ */
+@RestController
+public class ConsumerController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("testRibbon")
+    public String testRibbon(String serviceName){
+        /**
+         * restTemplate的bean被ribbon接管之后，ribbon会根据serverName去寻找提供者
+         * ribbon先拦截请求
+         * 再寻找主机名serviceName
+         * 借助eureka来做服务发现
+         * 然后根据负载均衡的算法来选取一个服务 获取ip 端口
+         * 最后发送请求
+         */
+        String result = restTemplate.getForObject("http://"+serviceName+"/hello",String.class);
+        return result;
+    }
+
+    /**
+     * 轮询算法
+     *
+     */
+
+
+}
---
+++ SpringCloud/ribbon/provider-b/src/main/java/com/example/providerb/controller/ProviderController.java
@@ -0,0 +1,18 @@
+package com.example.providerb.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author leezihong
+ * @date 2022/12/11 16:46
+ * @description
+ */
+@RestController
+public class ProviderController {
+
+    @GetMapping("/hello")
+    public String Hello(){
+        return "提供者B";
+    }
+}
